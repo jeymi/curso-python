dbdde76114c1cdf5ab4bf75a884156da18c4278a
Udem <udem@fedora.mshome.net>
2017-06-13 14:10:23 -0600

    programa de edad

---
+++ año.py
@@ -0,0 +1,6 @@
+person = input('ingresar el nombre de la persona:')
+edad = int(input('ingresar edad actual:'))
+fecha =int(input('ingresar año de nacimiento:'))
+resta = 100
+suma = resta + fecha
+print('en', suma, person, 'llegara a cumnplir 100 años')
