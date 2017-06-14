71da9f770b741d757f11f6cd0ad0f0468d6a5280
Udem <udem@fedora.mshome.net>
2017-06-14 17:49:15 -0600

    juego

---
+++ juegoppt.py
@@ -0,0 +1,20 @@
+from argparse import variable
+contador1=0
+contador2=0
+d = {'piedra':
+{ 'piedra': False,'papel':False,'tijera':True},
+'papel':
+{'piedra':True,'papel':False,'tijera':False},
+'tijera':
+{'piedra':False,'papel':True,'tijera':False}}
+
+while contador1<3 or contador2<3:
+	parser= argparse.argumentparsert('valor')
+	args= arg_parser()
+	random.choice(list(d.keys()))
+
+
+
+	
+
+
